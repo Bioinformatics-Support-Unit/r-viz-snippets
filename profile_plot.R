6c540a1266f23911736ef80acec1f6830d1bc64b
Simon Cockell <sjcockell@gmail.com>
2014-06-06 17:22:27 +0100

    Economised facet_split code

--- profile_plot.R
+++ profile_plot.R
@@ -21,33 +21,20 @@ require(reshape2)
 
 ##' This function plots a "GeneSpring" style profile plot from microarray data
 plot_profile <- function(expression_matrix, treatments=colnames(expression_matrix), sep=FALSE) {
-    
+
     ##' Rearrange the data matrix as a data frame, and add a column for grouping
     for_profile <- as.data.frame(expression_matrix)
     for_profile$probe <- row.names(for_profile)
     ##' melt the data into the long form (1 row per observation)
     melted <- melt(for_profile, id='probe')
+    ##'This provides the slot for 'melted' to determine how to split the facets
+    melted$facet_split = rep(treatments, each=nrow(for_profile))
     ##' order by the grouping factor (probe name)
     melted <- melted[with(melted, order(probe)), ]
     ##' Add an observation to colour by (as with GeneSpring plots, this is expression
-    ##' in the left-most array)  
+    ##' in the left-most array)
     melted$value_in_first = rep(melted[melted$probe==melted$probe & melted$variable==colnames(expression_matrix)[1],]$value,
                                 each=ncol(expression_matrix))
-  
-    ##'This provides the slot for 'melted' to determine how to split the facets
-    melted_var <- as.character(melted$variable)
-    unique_treatments <- unique(treatments)
-    tmp <- c()
-    for(i in 1:length(unique_treatments)) {
-      matches <- colnames(expression_matrix)[grep(unique_treatments[i], treatments)]
-      tmp <- c(tmp, matches)
-      for(j in 1:length(matches)) {
-        melted_var[melted_var == matches[j]] <- unique_treatments[i]
-      }
-      
-    }
-    melted$facet_split <- melted_var
-    
     ##' construct the plot in ggplot2, grouping by probe, and colouring by expression
     ##' in the first sample
     g <- ggplot(data=melted, aes(x=as.factor(variable), y=value)) +
@@ -56,11 +43,11 @@ plot_profile <- function(expression_matrix, treatments=colnames(expression_matri
                       theme(axis.text.x  = element_text(angle=90, vjust=0.5, size=10)) +
                       geom_point(aes(group=probe, colour=value_in_first)) +
                       geom_line(aes(group=probe, colour=value_in_first)) +
-                      scale_x_discrete(breaks=levels(factor(melted$variable)) , labels=treatments) +
-                      #scale_x_discrete(breaks=levels(factor(melted$variable)) , labels=treatment) + 
-                      scale_colour_gradient2(low='blue', high='red', mid='yellow', 
+                      scale_x_discrete(breaks=levels(factor(melted$variable))) +
+                      #scale_x_discrete(breaks=levels(factor(melted$variable)) , labels=treatment) +
+                      scale_colour_gradient2(low='blue', high='red', mid='yellow',
                                              midpoint=(max(melted$value_in_first) + min(melted$value_in_first))/2)
-    
+
     if(sep == TRUE){
       g <- g + facet_grid(. ~ facet_split, space="free", scales="free_x")
     }

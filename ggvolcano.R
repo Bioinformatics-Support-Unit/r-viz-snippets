b0f8183a1cf54a233471d8093f3a4f4a1d463945
Simon Cockell <sjcockell@gmail.com>
2014-06-18 22:18:51 +0100

    Added a ggvis Volcano Plot

--- ggvolcano.R
+++ ggvolcano.R
@@ -1,4 +1,6 @@
 require(ggplot2)
+require(scales)
+require(ggvis)
 
 gg_volcano = function(unfiltered_toptable, afc=2, pval=0.05) {
   # this function takes the unfiltered results of topTable (limma), and plots a
@@ -16,6 +18,32 @@ gg_volcano = function(unfiltered_toptable, afc=2, pval=0.05) {
     geom_vline(xintercept=log2(afc), colour="red", linetype=2) +
     theme_bw()
 }
+
+ggvis_volcano = function(unfiltered_toptable, afc=2, pval=0.05, tooltip="symbol") {
+  # this function takes the unfiltered results of topTable (limma), and plots a
+  # ggvis Volcano Plot with tooltips (genenames). 
+  # Optional arguments are an absolute fold change, adj
+  # p-Value cutoff and name of tooltip column in results
+  
+  # add a pass/fail column, based on the afc and pval cutoffs - 
+  # needs ot be numeric for colouring
+  unfiltered_toptable$pass = as.numeric(unfiltered_toptable$adj.P.Val < pval & abs(unfiltered_toptable$logFC) > log2(afc))
+  unfiltered_toptable[,"tooltip"] = unfiltered_toptable[,tooltip]
+  # Need data frames for ablines
+  h_abline = data.frame(logFC=range(unfiltered_toptable$logFC),adj.P.Val=pval)
+  v_abline1 = data.frame(logFC=-log2(afc),adj.P.Val=range(unfiltered_toptable$adj.P.Val))
+  v_abline2 = data.frame(logFC=log2(afc),adj.P.Val=range(unfiltered_toptable$adj.P.Val))
+  #build the plot
+  ggvis(data=NULL, x=~logFC, y=~-log10(adj.P.Val)) %>%
+    layer_points(size:=30, fill=~factor(pass),
+                 opacity:=0.5, key:=~tooltip, data=unfiltered_toptable) %>%
+    add_tooltip(function(x) x$tooltip) %>%
+    layer_paths(stroke:='red', data=h_abline) %>%
+    layer_paths(stroke:='red', data=v_abline1) %>%
+    layer_paths(stroke:='red', data=v_abline2)  
+}
+
+
 
 # sample usage
 gg_volcano(topTable(fit2, coef=1, number=Inf), 1.5, 0.01)
